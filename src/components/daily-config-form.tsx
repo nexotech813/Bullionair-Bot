0bf47242b77880b8179b406cab9d1d51863a7a1d
Nexo Tech <nexotech813@gmail.com>
2025-12-11 17:36:24 +0000

    Try fixing this error: `Build Error: Export useForm doesn't exist in tar

--- src/components/daily-config-form.tsx
+++ src/components/daily-config-form.tsx
@@ -1,6 +1,8 @@
+
 "use client";
 
-import { useForm, zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 import { Button } from "@/components/ui/button";
 import {
